c1b9fef1139dd7f96925b45bef2ba9a8607bc7b5
asimriazasim107@gmail.com <asimriazasim107@gmail.com>
2024-09-03 09:40:07 +0500

    added database

--- config/database.php
+++ config/database.php
@@ -51,7 +51,7 @@ return [
             'username' => env('MONGO_DB_USERNAME'),
             'password' => env('MONGO_DB_PASSWORD'),
             'options' => [
-                'database' => 'admin_yekbun'  
+                'database' => 'admin_yahala'  
             ]
         ],
 /*
