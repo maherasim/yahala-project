1228b24f55c024c95e8f94e469f892a1e7f3b476
asimriazasim107@gmail.com <asimriazasim107@gmail.com>
2024-09-11 09:13:45 +0500

    some changes

--- resources/views/content/authentications/login.blade.php
+++ resources/views/content/authentications/login.blade.php
@@ -39,7 +39,7 @@ $customizerHidden = 'customizer-hide';
               <span class="app-brand-logo demo login-logo">
                 {{-- @include('_partials.macros',["width"=>220]) --}}
 
-                    <img src = "{{asset('assets/svg/yahala-logo.jpg')}}" alt="Yahala"/>
+                    <img src = "{{asset('assets/svg/mylogo.jpeg')}}" alt="Yahala"/>
 
               </span>
               <!-- <span class="app-brand-text demo text-body fw-bolder">Yahala</span> -->
