6be8b2e330dc60df1850ee20e46949d40094b0f9
Asim Riaz <asimriazasim107@gmail.com>
2024-09-29 20:12:57 +0500

    fixed sigin model issue

--- app/Models/SignInSection.php
+++ app/Models/SignInSection.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model;
 
-class SigninSection extends Model
+class SignInSection extends Model
 {
     protected $connection = 'mongodb';
     use HasFactory;
