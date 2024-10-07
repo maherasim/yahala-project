080bc39ed0712516a7277572bcdb48da15f68d52
Asim Riaz <asimriazasim107@gmail.com>
2024-10-07 20:14:18 +0500

    added user section

--- resources/views/content/apps/app-user-view-account.blade.php
+++ resources/views/content/apps/app-user-view-account.blade.php
@@ -51,7 +51,7 @@
             <!-- User Pills -->
             <ul class="nav nav-pills flex-column flex-md-row mb-3">
                 <li class="nav-item"><a class="nav-link active" href="javascript:void(0);"><i
-                            class="bx bx-user me-1"></i>Follower</a></li>
+                            class="bx bx-user me-1"></i>Friends Family</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ url('app/user/'.$user->id.'/videos') }}"><i
                             class="bx bx-video me-1"></i>Videos</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ url('app/user/'.$user->id.'/activity') }}"><i
