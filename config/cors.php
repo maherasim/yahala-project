c1623f6664e3e18c3091a30f6661e907e73959c6
Asim Riaz <asimriazasim107@gmail.com>
2024-10-21 17:42:05 +0500

    added smpt

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,13 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'paths' => ['api/*'], // Allow CORS for all API routes
+    'allowed_methods' => ['*'], // Allow all HTTP methods
+    'allowed_origins' => ['https://yahala.yekbun.org'], // Allow your specific domain
     'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
+    'allowed_headers' => ['*'], // Allow all headers
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
