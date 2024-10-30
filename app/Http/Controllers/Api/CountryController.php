0a617e874a8676fbcbfc0f38b4a2c7db547bf93b
Asim Riaz <asimriazasim107@gmail.com>
2024-10-30 18:14:32 +0500

    added natioanlit

--- app/Http/Controllers/Api/CountryController.php
+++ app/Http/Controllers/Api/CountryController.php
@@ -37,18 +37,21 @@ class CountryController extends Controller
         ]);
     }
     
-	public function getNationality()
-	{
-		$nationality = Nationality::all(); 
-	
-		 
-		return response()->json([
-			'success' => true,
-			'message' => 'Nationalities retrieved successfully.',
-			'data' => $nationality,
-		]);
-	}
-
+    public function getNationality()
+    {
+        $nationalities = Nationality::all()->map(function($nationality) {
+            // Use the `storage_path` helper to create the correct URL
+            $nationality->thumbnail_path = asset('storage/' . $nationality->thumbnail_path); // Ensure correct path
+            return $nationality;
+        });
+    
+        return response()->json([
+            'success' => true,
+            'message' => 'Nationalities retrieved successfully.',
+            'data' => $nationalities,
+        ]);
+    }
+    
 
     public function search_location(Request $request)
     {
