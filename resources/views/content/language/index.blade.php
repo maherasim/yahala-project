790f823046a983cb0de61a707c614f8fcae26352
Asim Riaz <asimriazasim107@gmail.com>
2025-01-06 21:32:50 +0500

    added

--- resources/views/content/language/index.blade.php
+++ resources/views/content/language/index.blade.php
@@ -2429,22 +2429,7 @@ foreach ($app_policyFields as $field3) {
                                                                 </div>
                                                             </div>
                                     
-                                                            <div class="row mt-2">
-                                                                <div class="col-md-6">
-                                                                    <h6>Your First Name</h6>
-                                                                </div>
-                                                                <div class="col-md-6">
-                                                                    <input type="text" class="form-control" name="your_first_name" value="{{ $signupsection->your_first_name ?? '' }}" placeholder="Your your_first_name">
-                                                                </div>
-                                                            </div>
-                                                            <div class="row mt-2">
-                                                                <div class="col-md-6">
-                                                                    <h6>Your Last Name</h6>
-                                                                </div>
-                                                                <div class="col-md-6">
-                                                                    <input type="text" class="form-control" name="your_last_name" value="{{ $signupsection->your_last_name ?? '' }}" placeholder=" your_last_name">
-                                                                </div>
-                                                            </div>
+                                                            <!-- Username -->
                                                             <div class="row mt-2">
                                                                 <div class="col-md-6">
                                                                     <h6>Your Username</h6>
