5c4c359ff460b0da263ba329a81321e436e48cb2
Asim Riaz <asimriazasim107@gmail.com>
2025-01-21 08:59:27 +0500

    permission seeder

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -4,9 +4,12 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+// use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Hash;
+// use Spatie\Permission\Models\Permission;
 use Maklad\Permission\Models\Role;
 use Maklad\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Support\Facades\DB;
 
 class PermissionsSeeder extends Seeder
@@ -18,30 +21,20 @@ class PermissionsSeeder extends Seeder
      */
     public function run()
     {
-        // Path to the JSON file
         $jsonFile = database_path('data/permissions.json');
-
-        // Read the JSON file
-        if (!file_exists($jsonFile)) {
-            $this->command->error("JSON file not found at {$jsonFile}");
-            return;
+        $jsonData = json_decode(file_get_contents($jsonFile), true);
+    
+        foreach ($jsonData as $permission) {
+            // Remove '_id' field if it exists to avoid modification of the immutable field
+            unset($permission['_id']);
+            
+            DB::connection('mongodb')->collection('permissions')->updateOrInsert(
+                ['name' => $permission['name'], 'guard_name' => $permission['guard_name']],
+                $permission
+            );
         }
-
-        $jsonData = file_get_contents($jsonFile);
-
-        // Decode JSON into an array
-        $permissions = json_decode($jsonData, true);
-
-        if (json_last_error() !== JSON_ERROR_NONE) {
-            $this->command->error('Invalid JSON format.');
-            return;
-        }
-
-        DB::connection('mongodb')->collection('permissions')->delete();
-
-        // Insert the data into the 'permissions' collection
-        DB::connection('mongodb')->collection('permissions')->insert($permissions);
-
-        $this->command->info('Permissions data seeded successfully.');
     }
+    
+    
+    
 }
