3ec70b8a84358a058a63ebf5f2c9142690a89d25
Asim Riaz <asimriazasim107@gmail.com>
2025-02-21 16:02:18 +0500

    setting section

--- resources/views/settingsection.blade.php
+++ resources/views/settingsection.blade.php
@@ -24,72 +24,43 @@ $settingsection = App\Models\SectionSetting::where('language_id', $language->id)
                             </div>
                         </div>
 
-                        <!-- My Account -->
-                        <div class="row mt-4">
-                            <div class="col-md-12">
-                                <h6>My Account</h6>
-                                <label for="setNewPassword">Set New Password:</label>
-                                <input type="text" id="setNewPassword" name="setNewPassword" class="form-control" value="{{ $settingsection->setNewPassword ?? '' }}">
-                                <br>
-                                <label for="oldPassword">Old Password:</label>
-                                <input type="text" id="oldPassword" name="oldPassword" class="form-control" value="{{ $settingsection->oldPassword ?? '' }}">
-                                <br>
-                                <label for="newPassword">New Password:</label>
-                                <input type="text" id="newPassword" name="newPassword" class="form-control" value="{{ $settingsection->newPassword ?? '' }}">
-                                <br>
-                                <label for="confirmNewPassword">Confirm New Password:</label>
-                                <input type="text" id="confirmNewPassword" name="confirmNewPassword" class="form-control" value="{{ $settingsection->confirmNewPassword ?? '' }}">
-                                <label for="email">E-Mail:</label>
-                                <input type="text" id="email" name="email" class="form-control" value="{{ $settingsection->email ?? '' }}">
-                                <br>
-                                <label for="oldEmail">Old E-Mail:</label>
-                                <input type="text" id="oldEmail" name="oldEmail" class="form-control" value="{{ $settingsection->oldEmail ?? '' }}">
-                                <br>
-                                <label for="newEmail">New E-Mail:</label>
-                                <input type="text" id="newEmail" name="newEmail" class="form-control" value="{{ $settingsection->newEmail ?? '' }}">
-                                <br>
-                                <label for="repeatNewEmail">Repeat New E-Mail:</label>
-                                <input type="text" id="repeatNewEmail" name="repeatNewEmail" class="form-control" value="{{ $settingsection->repeatNewEmail ?? '' }}">
-                                <label for="details">Details:</label>
-                                <input type="text" id="details" name="details" class="form-control" value="{{ $settingsection->details ?? '' }}">
-                                <br>
-                                <label for="status">Your Status:</label>
-                                <input type="text" id="status" name="status" class="form-control" value="{{ $settingsection->status ?? '' }}">
-                                <label for="notificationType">Notification Type:</label>
-                                <input type="text" id="notificationType" name="notificationType" class="form-control" value="{{ implode(',', $settingsection->notificationType ?? []) }}">
-                                <label for="musicSetting">Music Setting:</label>
-                                <input type="text" id="musicSetting" name="musicSetting" class="form-control" value="{{ implode(',', $settingsection->musicSetting ?? []) }}">
-                                <br>
-                                <label for="musicVolume">Music Volume:</label>
-                                <input type="text" id="musicVolume" name="musicVolume" class="form-control" value="{{ $settingsection->musicVolume ?? 50 }}">
-                                <label for="messagesRingtone">Messages:</label>
-                                <input type="text" id="messagesRingtone" name="messagesRingtone" class="form-control" value="{{ $settingsection->messagesRingtone ?? '' }}"><br>
-                                <label for="callRingtone">Call:</label>
-                                <input type="text" id="callRingtone" name="callRingtone" class="form-control" value="{{ $settingsection->callRingtone ?? '' }}"><br>
-                                <label for="notificationsRingtone">Notifications:</label>
-                                <input type="text" id="notificationsRingtone" name="notificationsRingtone" class="form-control" value="{{ $settingsection->notificationsRingtone ?? '' }}">
-                                <label for="leaveReason">Leave Reason:</label>
-                                <input type="text" id="leaveReason" name="leaveReason" class="form-control" value="{{ implode(',', $settingsection->leaveReason ?? []) }}">
-                                <br>
-                                <label for="describeReason">Describe:</label>
-                                <input type="text" id="describeReason" name="describeReason" class="form-control" value="{{ $settingsection->describeReason ?? '' }}">
-                                <label for="contactType">Contact Type:</label>
-                                <input type="text" id="contactType" name="contactType" class="form-control" value="{{ $settingsection->contactType ?? '' }}">
-                                <br>
-                                <label for="message">Message:</label>
-                                <input type="text" id="message" name="message" class="form-control" value="{{ $settingsection->message ?? '' }}">
-                                 
-                               
-                                
+                        <!-- Form Fields -->
+                        @foreach ([
+                            'setNewPassword' => 'Set New Password',
+                            'oldPassword' => 'Old Password',
+                            'newPassword' => 'New Password',
+                            'confirmNewPassword' => 'Confirm New Password',
+                            'email' => 'E-Mail',
+                            'oldEmail' => 'Old E-Mail',
+                            'newEmail' => 'New E-Mail',
+                            'repeatNewEmail' => 'Repeat New E-Mail',
+                            'details' => 'Details',
+                            'status' => 'Your Status',
+                            'notificationType' => 'Notification Type',
+                            'musicSetting' => 'Music Setting',
+                            'musicVolume' => 'Music Volume',
+                            'messagesRingtone' => 'Messages Ringtone',
+                            'callRingtone' => 'Call Ringtone',
+                            'notificationsRingtone' => 'Notifications Ringtone',
+                            'leaveReason' => 'Leave Reason',
+                            'describeReason' => 'Describe Reason',
+                            'contactType' => 'Contact Type',
+                            'message' => 'Message'
+                        ] as $name => $label)
+                        <div class="row mt-2">
+                            <div class="col-md-6">
+                                <h6>{{ $label }}</h6>
                             </div>
-                            
+                            <div class="col-md-6">
+                                <input type="text" id="{{ $name }}" name="{{ $name }}" class="form-control" value="{{ $settingsection->$name ?? '' }}">
+                            </div>
                         </div>
-
-                        
- 
- 
+                        @endforeach
                     </div>
-
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-label-secondary"
+                            data-bs-dismiss="modal">Save</button>
+                    </div>
                 </form>
             </div>
         </div>
