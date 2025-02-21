0803c2b7b727070405a38e092687805fb12eee46
Asim Riaz <asimriazasim107@gmail.com>
2025-02-21 15:49:42 +0500

    setting section

--- resources/views/settingsection.blade.php
+++ resources/views/settingsection.blade.php
@@ -1,7 +1,7 @@
 @php
 $settingsection = App\Models\SectionSetting::where('language_id', $language->id)->first();
 @endphp
-<!-- resources/views/edit_footer_quick_section_modal.blade.php -->
+
 <div class="modal fade" id="settingsectionsok{{ $language->id }}" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-md modal-dialog-centered" role="document">
         <div class="modal-content">
@@ -60,27 +60,15 @@ $settingsection = App\Models\SectionSetting::where('language_id', $language->id)
                             <div class="col-md-6">
                                 <h6>User Details</h6>
                                 <label for="details">Details:</label>
-                                <textarea id="details" name="details" class="form-control">{{ $settingsection->details ?? '' }}</textarea>
+                                <input type="text" id="details" name="details" class="form-control" value="{{ $settingsection->details ?? '' }}">
                                 <br>
-                                <label>Your Status:</label><br>
-                                <input type="radio" id="single" name="status" value="single" {{ isset($settingsection->status) && $settingsection->status == 'single' ? 'checked' : '' }}>
-                                <label for="single">Single</label><br>
-                                <input type="radio" id="engaged" name="status" value="engaged" {{ isset($settingsection->status) && $settingsection->status == 'engaged' ? 'checked' : '' }}>
-                                <label for="engaged">Engaged</label><br>
-                                <input type="radio" id="married" name="status" value="married" {{ isset($settingsection->status) && $settingsection->status == 'married' ? 'checked' : '' }}>
-                                <label for="married">Married</label>
+                                <label for="status">Your Status:</label>
+                                <input type="text" id="status" name="status" class="form-control" value="{{ $settingsection->status ?? '' }}">
                             </div>
                             <div class="col-md-6">
-                                <!-- Notifications -->
                                 <h6>Notifications</h6>
-                                <input type="checkbox" id="newUploads" name="notificationType[]" value="newUploads" {{ isset($settingsection->notificationType) && in_array('newUploads', $settingsection->notificationType) ? 'checked' : '' }}>
-                                <label for="newUploads">New Uploads</label><br>
-                                <input type="checkbox" id="playMusic" name="notificationType[]" value="playMusic" {{ isset($settingsection->notificationType) && in_array('playMusic', $settingsection->notificationType) ? 'checked' : '' }}>
-                                <label for="playMusic">Play Music</label><br>
-                                <input type="checkbox" id="videoCall" name="notificationType[]" value="videoCall" {{ isset($settingsection->notificationType) && in_array('videoCall', $settingsection->notificationType) ? 'checked' : '' }}>
-                                <label for="videoCall">Video Call</label><br>
-                                <input type="checkbox" id="videoPlay" name="notificationType[]" value="videoPlay" {{ isset($settingsection->notificationType) && in_array('videoPlay', $settingsection->notificationType) ? 'checked' : '' }}>
-                                <label for="videoPlay">Video Play</label>
+                                <label for="notificationType">Notification Type:</label>
+                                <input type="text" id="notificationType" name="notificationType" class="form-control" value="{{ implode(',', $settingsection->notificationType ?? []) }}">
                             </div>
                         </div>
 
@@ -88,13 +76,13 @@ $settingsection = App\Models\SectionSetting::where('language_id', $language->id)
                         <div class="row mt-4">
                             <div class="col-md-6">
                                 <h6>Music</h6>
-                                <input type="checkbox" id="playInBackground" name="musicSetting[]" value="playInBackground" {{ isset($settingsection->musicSetting) && in_array('playInBackground', $settingsection->musicSetting) ? 'checked' : '' }}>
-                                <label for="playInBackground">Play in Background</label><br>
+                                <label for="musicSetting">Music Setting:</label>
+                                <input type="text" id="musicSetting" name="musicSetting" class="form-control" value="{{ implode(',', $settingsection->musicSetting ?? []) }}">
+                                <br>
                                 <label for="musicVolume">Music Volume:</label>
-                                <input type="range" id="musicVolume" name="musicVolume" min="0" max="100" class="form-control" value="{{ $settingsection->musicVolume ?? 50 }}">
+                                <input type="text" id="musicVolume" name="musicVolume" class="form-control" value="{{ $settingsection->musicVolume ?? 50 }}">
                             </div>
                             <div class="col-md-6">
-                                <!-- Ringtone -->
                                 <h6>Ringtone</h6>
                                 <label for="messagesRingtone">Messages:</label>
                                 <input type="text" id="messagesRingtone" name="messagesRingtone" class="form-control" value="{{ $settingsection->messagesRingtone ?? '' }}"><br>
@@ -109,29 +97,21 @@ $settingsection = App\Models\SectionSetting::where('language_id', $language->id)
                         <div class="row mt-4">
                             <div class="col-md-6">
                                 <h6>Disable Account</h6>
-                                <input type="checkbox" id="noLanguage" name="leaveReason[]" value="noLanguage" {{ isset($settingsection->leaveReason) && in_array('noLanguage', $settingsection->leaveReason) ? 'checked' : '' }}>
-                                <label for="noLanguage">Doesn’t contain my Language</label><br>
-                                <input type="checkbox" id="noMusic" name="leaveReason[]" value="noMusic" {{ isset($settingsection->leaveReason) && in_array('noMusic', $settingsection->leaveReason) ? 'checked' : '' }}>
-                                <label for="noMusic">Doesn’t contain Music I look for</label><br>
-                                <input type="checkbox" id="notInterested" name="leaveReason[]" value="notInterested" {{ isset($settingsection->leaveReason) && in_array('notInterested', $settingsection->leaveReason) ? 'checked' : '' }}>
-                                <label for="notInterested">Not what I look for elsewhere</label><br><br>
-                                <label for="describeReason">Describe:</label><br>
-                                <textarea id="describeReason" name="describeReason" class="form-control">{{ $settingsection->describeReason ?? '' }}</textarea>
+                                <label for="leaveReason">Leave Reason:</label>
+                                <input type="text" id="leaveReason" name="leaveReason" class="form-control" value="{{ implode(',', $settingsection->leaveReason ?? []) }}">
+                                <br>
+                                <label for="describeReason">Describe:</label>
+                                <input type="text" id="describeReason" name="describeReason" class="form-control" value="{{ $settingsection->describeReason ?? '' }}">
                             </div>
                             <div class="col-md-6">
-                                <!-- Policy and terms -->
-                                <h6>Policy and terms</h6>
+                                <h6>Policy and Terms</h6>
                                 <p>Display policy and terms here</p>
-                                <!-- Contact -->
                                 <h6>Contact</h6>
-                                <label for="contactType">Contact us:</label><br>
-                                <select id="contactType" name="contactType" class="form-control">
-                                    <option value="general" {{ isset($settingsection->contactType) && $settingsection->contactType == 'general' ? 'selected' : '' }}>General Inquiry</option>
-                                    <option value="support" {{ isset($settingsection->contactType) && $settingsection->contactType == 'support' ? 'selected' : '' }}>Support</option>
-                                    <option value="feedback" {{ isset($settingsection->contactType) && $settingsection->contactType == 'feedback' ? 'selected' : '' }}>Feedback</option>
-                                </select><br>
-                                <label for="message">Type your Message here:</label><br>
-                                <textarea id="message" name="message" class="form-control">{{ $settingsection->message ?? '' }}</textarea>
+                                <label for="contactType">Contact Type:</label>
+                                <input type="text" id="contactType" name="contactType" class="form-control" value="{{ $settingsection->contactType ?? '' }}">
+                                <br>
+                                <label for="message">Message:</label>
+                                <input type="text" id="message" name="message" class="form-control" value="{{ $settingsection->message ?? '' }}">
                             </div>
                         </div>
 
@@ -142,7 +122,6 @@ $settingsection = App\Models\SectionSetting::where('language_id', $language->id)
                                 <p>Display portal information and address here</p>
                             </div>
                             <div class="col-md-6">
-                                <!-- Logout -->
                                 <h6>Logout</h6>
                                 <label>Are you sure to logout from Yahala?</label><br>
                                 <button type="submit" class="btn btn-secondary">Save</button>
