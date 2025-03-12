092ec895c96a1ada91f01ace7b0282d5859ffea9
Asim Riaz <asimriazasim107@gmail.com>
2025-03-13 00:21:27 +0500

    added

--- app/Http/Controllers/Api/AvatarsController.php
+++ app/Http/Controllers/Api/AvatarsController.php
@@ -26,7 +26,7 @@ class AvatarsController extends Controller
 	 {
 		 try {
 			 $avatars = Avatars_Feed::all();
-	 dd( $avatars);
+	 dd( 'asim',$avatars);
 			 return response()->json([
 				 'message' => 'Ok',
 				 'data' => $avatars
